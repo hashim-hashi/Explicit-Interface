6e200d350f35fab1143c7cd9df4a9296b7800887
Hashim <hashimhashi010@gmail.com>
2022-12-23 17:31:47 +0530

    Add project files.

---
+++ Interface/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+interface I1
+{
+    void InterfaceMethod();
+}
+interface I2
+{
+    void InterfaceMethod();
+}
+
+public class Program : I1, I2
+{
+    void I1.InterfaceMethod()
+    {
+        Console.WriteLine("I1 method");
+    }
+    void I2.InterfaceMethod()
+    {
+        Console.WriteLine("I2 method");
+    }
+    public static void Main(string[] args)
+    {
+
+        Program P = new Program();
+        ((I1)P).InterfaceMethod();
+        ((I2)P).InterfaceMethod();
+    }
+}
